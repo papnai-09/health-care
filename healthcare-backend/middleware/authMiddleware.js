9781070979203eba049764d4e6ca4d1a76a7a32f
Gaurav Papnai <134196969+papnai-09@users.noreply.github.com>
2026-05-09 11:27:54 +0530

    Add files via upload

---
+++ healthcare-backend/middleware/authMiddleware.js
@@ -0,0 +1,83 @@
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+exports.protect = async (req, res, next) => {
+
+  try {
+
+    let token;
+
+    if (
+
+      req.headers.authorization &&
+
+      req.headers.authorization.startsWith('Bearer')
+
+    ) {
+
+      token =
+        req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+
+      return res.status(401).json({
+
+        success: false,
+
+        message: 'Not Authorized'
+
+      });
+    }
+
+    const decoded =
+      jwt.verify(
+
+        token,
+
+        process.env.JWT_SECRET
+      );
+
+    req.user =
+      await User.findById(decoded.id)
+      .select('-password');
+
+    next();
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(401).json({
+
+      success: false,
+
+      message: 'Invalid Token'
+
+    });
+  }
+};
+
+exports.authorize = (...roles) => {
+
+  return (req, res, next) => {
+
+    if (
+
+      !roles.includes(req.user.role)
+
+    ) {
+
+      return res.status(403).json({
+
+        success: false,
+
+        message: 'Access Denied'
+
+      });
+    }
+
+    next();
+  };
+};
