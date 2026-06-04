1524be0b95d7dff438af5880942637a36e96e65a
Gaurav Papnai <134196969+papnai-09@users.noreply.github.com>
2026-06-04 10:01:06 +0530

    Delete healthcare-backend directory

--- healthcare-backend/routes/prescriptionRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { createPrescription } = require('../controllers/prescriptionController');
-
-router.post('/', createPrescription);
-
-module.exports = router;
